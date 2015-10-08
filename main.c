e31a01c57dc5c9e59c6fbae3a64d6f92c15b6c71
abaquar <balacamasnaocai@gmail.com>
2015-10-08 11:17:50 -0300

    Capacitação C
    Código final da capacitação

---
+++ main.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <conio.h>
+
+/* Definições *****************************************************************/
+
+#define LARGURA_TELA 79
+#define ALTURA_TELA  24
+#define FALSE       0
+#define TRUE        1
+
+typedef struct{         //Estrutura dos tiles do mapa
+    char c;             //c          -> Caracter que representa o tile
+    short passavel;     //passavel   -> Determina se entidades podem andar por cima do tile
+}TILE_STRUCT;
+const TILE_STRUCT TILE[] = {  //Dicionário com todas as informações sobre tiles
+    {'.', TRUE},        //TILE[0] -> Chão
+    {'#', FALSE},       //TILE[1] -> Parede
+};
+
+/* Variáveis Globais **********************************************************/
+
+char TELA[LARGURA_TELA*ALTURA_TELA];        //Matriz com o que será impresso na saída padrão
+short MAPA_ATUAL[LARGURA_TELA*ALTURA_TELA]; //Matriz com ids dos tiles do mapa atual. Por convenção o mapa sempre é do tamanho da tela
+
+/* Funções de Mapa ************************************************************/
+void renderizaMapa(){
+    /* Renderiza o mapa atual na tela */
+    short i;
+    for(i=0;i<LARGURA_TELA*ALTURA_TELA;i++){
+        TELA[i] = TILE[MAPA_ATUAL[i]].c;
+    }
+}
+
+void inicializaMapa(){
+    /* Inicializa o mapa com paredes em volta */
+    short x, y;
+    for(y=0;y<ALTURA_TELA;y++){
+        for(x=0;x<LARGURA_TELA;x++){
+            if(0==x || LARGURA_TELA-1==x || 0==y || ALTURA_TELA-1==y){
+                MAPA_ATUAL[y*LARGURA_TELA+x] = 1;
+            }else{
+                MAPA_ATUAL[y*LARGURA_TELA+x] = 0;
+            }
+        }
+    }
+}
+
+/* Funções de Tela ************************************************************/
+void imprimeTela(){
+    /* Mostra a tela na saída padrão*/
+    short x, y;
+    for(y=0;y<ALTURA_TELA;y++){
+        for(x=0;x<LARGURA_TELA;x++){
+            printf("%c",TELA[y*LARGURA_TELA + x]);
+        }
+        printf("\n");
+    }
+}
+
+/* Outras Funções *************************************************************/
+
+int esperaTecla(){
+    /* Espera até que o usuário aperte uma tecla e retorna o código da tecla pressionada */
+    return getch();
+}
+
+/* Main ***********************************************************************/
+int main(int argc, char** argv) {
+    inicializaMapa(); //TODO: Excluir esta função e fazer a geração procedural dos mapas do jogo
+
+    renderizaMapa();
+    imprimeTela();
+
+    esperaTecla();
+    return (EXIT_SUCCESS);
+}
